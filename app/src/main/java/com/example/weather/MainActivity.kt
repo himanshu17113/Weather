7094ab65466d0602186ad685393383d011870a42
himanshu17113 <98476250+himanshu17113@users.noreply.github.com>
2023-01-10 23:14:42 +0530

    cleanup

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -155,42 +155,6 @@ class MainActivity : AppCompatActivity() {
                         findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
                     }}
 
-
-//                  {
-//
-//
-//                        binding.city.text = weathers.name
-//
-//                        var lat = weathers.coord.lat
-//                        var long = weathers.coord.lon
-//                        binding.coordinates.text = "${lat} , ${long}"
-//                        binding.weather.text = weathers.weather[0].main
-//                        var tempr = weathers.main.temp
-//
-//                        binding.temp.text = "${tempr}°F"
-//
-//
-//                        var mintemp = weathers.main.temp_min
-//
-//                        min_temp.text = "${mintemp}°F"
-//                        var maxtemp = weathers.main.temp_max
-//
-//                        max_temp.text = "${maxtemp}°F"
-//
-//                        binding.pressure.text = weathers.main.pressure.toString()
-//                        binding.humidity.text = weathers.main.humidity.toString()
-//                        binding.wind.text = weathers.wind.speed.toString()
-//
-//                        degree.text = "Degree : " + weathers.wind?.deg.toString() + "°"
-//
-////        gust.text="Gust : "+response.getJSONObject("wind").getString("gust")
-//                    }
-//                                   else{
-//                                       loadData(
-//
-//                                       )
-//                                   }
-
                 }
 
                 override fun onFailure(call: Call<data>, t: Throwable) {
@@ -198,85 +162,6 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this@MainActivity,"open net", Toast.LENGTH_LONG).show()
                 }
 
-
-
             } )
-
-
-
     }
-
-
-
-//
-//    inner class weatherTask() : AsyncTask<String, Void, String>() {
-//        override fun onPreExecute() {
-//            super.onPreExecute()
-//            /* Showing the ProgressBar, Making the main design GONE */
-//            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
-//            findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
-//            findViewById<TextView>(R.id.errorText).visibility = View.GONE
-//        }
-//
-//        override fun doInBackground(vararg params: String?): String? {
-//            var response:String?
-//            try{
-//                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(
-//                    Charsets.UTF_8
-//                )
-//            }catch (e: Exception){
-//                response = null
-//            }
-//            return response
-//        }
-//
-//        override fun onPostExecute(result: String?) {
-//            super.onPostExecute(result)
-////            try {
-////                /* Extracting JSON returns from the API */
-////                val jsonObj = JSONObject(result)
-////                val main = jsonObj.getJSONObject("main")
-////                val sys = jsonObj.getJSONObject("sys")
-////                val wind = jsonObj.getJSONObject("wind")
-////                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
-////
-////                val updatedAt:Long = jsonObj.getLong("dt")
-////                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
-////                val temp = main.getString("temp")+"°C"
-////                val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
-////                val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
-////                val pressure = main.getString("pressure")
-////                val humidity = main.getString("humidity")
-////
-////                val sunrise:Long = sys.getLong("sunrise")
-////                val sunset:Long = sys.getLong("sunset")
-////                val windSpeed = wind.getString("speed")
-////                val weatherDescription = weather.getString("description")
-////
-////                val address = jsonObj.getString("name")+", "+sys.getString("country")
-////
-////                /* Populating extracted data into our views */
-////                findViewById<TextView>(R.id.address).text = address
-////                findViewById<TextView>(R.id.updated_at).text =  updatedAtText
-////                findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
-////                findViewById<TextView>(R.id.temp).text = temp
-////                findViewById<TextView>(R.id.temp_min).text = tempMin
-////                findViewById<TextView>(R.id.temp_max).text = tempMax
-////                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
-////                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
-////                findViewById<TextView>(R.id.wind).text = windSpeed
-////                findViewById<TextView>(R.id.pressure).text = pressure
-////                findViewById<TextView>(R.id.humidity).text = humidity
-////
-////                /* Views populated, Hiding the loader, Showing the main design */
-////                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-////                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
-////
-////            } catch (e: Exception) {
-////                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-////                findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
-////            }
-//
-//        }
-//    }
 }
